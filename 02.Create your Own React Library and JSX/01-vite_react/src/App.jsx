cfe0d2ce06bc48e61ddf67a7e5f1205070e97571
Arnav Sagar <arnav.07.sagar@gmail.com>
2025-03-22 14:02:18 +0530

    final of customReact

--- 02.Create your Own React Library and JSX/01-vite_react/src/App.jsx
+++ 02.Create your Own React Library and JSX/01-vite_react/src/App.jsx
@@ -6,14 +6,15 @@ import Chai from './Chai'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
+  const username = "by Arnav Sagar"; // note: this variable can be shown in the return inside an  {}-> expression
   return (
   <>
     <div>
 {/*  Rule 1:In React, components are functions that return JSX, and when you use a component inside another component, you must write it as <Chai /> instead of just Chai() or <chai />. for more info goto Chai.jsx
   Rule 2: Also you can only return one element and if you need to return more than one element you can use "<div>" or just "<>"-fragment tag make a to fo it as shown down */}
         <Chai/> 
-         <h2> Chai making in React</h2>
+         <h2> Chai making in React {username /* this is an expression*/}</h2>
     </div>
     <h3>Chai aur React</h3>
   </> 
